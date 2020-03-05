b7dc8627187d40201efe44ff53c6ca0bba5bf984
MattHDwyer <matthew.h.dwyer@gmail.com>
2020-03-06 08:41:02 +1100

    added README and updated AddMovies API

---
+++ README.md
@@ -0,0 +1,9 @@
+# MY PHP LEARNING
+This repo is me currently learning php. 
+
+## PHPSANDBOX:
+All these files are from a tutorial, just following along and doing that. 
+
+## JSON PARSER:
+All the files in this folder are apart of the API that I hit for my <a href="https://github.com/MattHDwyer/React-Shopping-Cart">React Shopping Cart App</a>
+
--- jsonparser/post/movie_items.php
+++ jsonparser/post/movie_items.php
@@ -3,22 +3,23 @@
 	$jsondata = file_get_contents($jsonfile);
 	$json = json_decode($jsondata, true);
 
-
-
-{
-		$title = htmlentities($_POST['title']);
-		$year = htmlentities($_POST['year']);
-		$price = htmlentities($_POST['price']);
-		$price = filter_var($price, FILTER_VALIDATE_FLOAT);
-		$genre = htmlentities($_POST['genre']);
-		$synopsis = htmlentities($_POST['synopsis']);
-		
+	$response_body = file_get_contents('php://input');
+	echo $response_body;
+	$response_decode = json_decode($response_body, true);
+	
+	$title = htmlentities($response_decode['title'] /*??""  shorthand for isset */ );
+	$year = htmlentities($response_decode['year']);
+	$price = htmlentities($response_decode['price']);
+	$price = filter_var($price, FILTER_VALIDATE_FLOAT);
+	$genre = htmlentities($response_decode['genre']);
+	$synopsis = htmlentities($response_decode['synopsis']);
+	
+	if($title != null && $year != null && $price != null && $genre != null && $synopsis != null) {
 		$arr = array('title'=>$title, 'year'=>$year, 'price'=>$price, 'genre'=>$genre, 'synopsis'=>$synopsis);
-
-		// var_dump(json_encode($arr,JSON_PRETTY_PRINT));
 		array_push($json, $arr);
 		file_put_contents($jsonfile, json_encode($json, JSON_PRETTY_PRINT));
 		echo "Post successful";
 	}
-	echo "API HIT"
+
+	
 ?>
